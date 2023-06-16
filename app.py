d749b3f93e159fc688b4989776ad5a2e184e6963
Khaled Eldifrawy <73159214+khaledeldifrawy@users.noreply.github.com>
2023-06-16 21:00:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from nltk_utils import tokenize, stem, bag_of_ward
 from train import all_word,tags,xy,set_state
 
 model = load_model("model.h5")
-intents = json.loads(open("dataset.json").read())
+intents = json.loads(open("new-dataset.json").read())
 
 app = Flask(__name__)
 
