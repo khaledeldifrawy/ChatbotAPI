42e892bfd51b4db1b1dfd0f9f4c72dfce80fc7bb
Khaled Eldifrawy <73159214+khaledeldifrawy@users.noreply.github.com>
2023-06-20 20:49:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import numpy as np
 import pickle
+import joblib
 import json
 from keras.models import load_model
 from nltk_utils import tokenize, stem, bag_of_ward
@@ -8,11 +9,18 @@ from train import all_word,tags,xy,set_state
 
 app = Flask(__name__)
 
-model = load_model("model.h5")
+
+
+# tokenizer_q
+tokenizer_q1 = joblib.load("tokenizer_q")
+# tokenizer_a
+tokenizer_a1 = joblib.load("tokenizer_a")
+
+model = load_model("grad.h5")
 intents = json.loads(open("new-dataset.json").read())
 
 
-depreesion = ' \n 1. Practice Self-Care: Taking care of your physical and emotional needs is important for managing depression. This can include getting enough sleep, eating a balanced diet, exercising regularly, and engaging in activities that bring you joy. \n 2. Seek professional help: Depression is a serious condition that may require medical attention. It is important to speak with a mental health professional who can provide guidance and support in managing your symptoms.'
+depreesion = ' \n 1. Practice Self-Care: Taking care of your physical and emotional needs is important for managing depression. This can include getting enough sleep, eating a balanced diet, exercising regularly, and engaging in activities that bring you joy. \n\n2. Seek professional help: Depression is a serious condition that may require medical attention. It is important to speak with a mental health professional who can provide guidance and support in managing your symptoms.'
 Anxiety = ["1.Identify triggers: Work with your therapist to identify situations, people, or events that trigger your anxiety. This can help you prepare and develop coping strategies.","2.Gradual exposure: Gradual exposure to feared situations or triggers can be an effective way to manage anxiety. Work with a therapist to develop a gradual exposure plan that feels safe and manageable for you.","3.Cognitive behavioral therapy (CBT): is one of the most effective forms of psychotherapy for anxiety disorders. In general, short-term cognitive behavioral therapy focuses on teaching specific skills to improve symptoms and gradually returning to activities the person avoids because of anxiety.","4.Use positive self-talk: Replace negative self-talk with positive, supportive statements. This can help reduce anxiety and increase self-confidence.","5.Avoid procrastination: Procrastination can increase anxiety by creating a sense of urgency and stress. Try to complete tasks as they come up, and break larger tasks into smaller, more manageable steps.","6.Learn coping skills: Your therapist can teach you specific coping skills, such as cognitive-behavioral techniques or exposure therapy, to help manage your anxiety.","7.Seek professional help: Anxiety disorders can be debilitating and it's important to speak with a mental health professional who can provide guidance and support in managing your symptoms.","8.Practice relaxation techniques: Relaxation techniques like deep breathing, meditation, or yoga can help manage stress and reduce symptoms of anxiety.","9.Challenge negative thoughts: Anxiety can often involve negative and catastrophic thinking. Practice challenging these thoughts by looking for evidence that supports or contradicts them, and reframing them in a more positive and realistic light.","10.Maintain a healthy lifestyle: Taking care of your physical and emotional needs is important for managing anxiety. This can include getting enough sleep, eating a balanced diet, and exercising regularly.","Send a message if there are any inquiries regarding these instructions or if there is difficulty in applying them."]
 Addictive = ["1.Create a support system: Building a support system of family, friends, or support groups can be helpful in managing addiction. Reach out to loved ones and consider joining a support group such as Alcoholics Anonymous or Narcotics Anonymous.","2.Create a recovery plan: Work with your therapist or support group to create a recovery plan that includes goals, strategies, and a support system.","3.Setting boundaries: Setting boundaries with people or situations that enable or lead to addiction can be helpful in managing addiction. Clearly articulate your boundaries and stick to them.","4.Be accountable: Accountability can be helpful in managing addiction. Consider sharing your recovery plan with a trusted friend or family member and asking them to check in with you regularly.","5.Celebrate small victories: Addiction recovery is a long and difficult process. Celebrate the small victories along the way, like completing a week of sobriety or reaching a milestone in your recovery plan.","6.Attending support group meetings: Support group meetings, such as Alcoholics Anonymous or Narcotics Anonymous, can provide a safe and supportive space to share experiences and learn from others who have gone through similar struggles.","7.Motivational Enhancement Therapy (MET): Motivational therapy is an approach that helps increase people's willingness to change. It can be useful for improving adherence and motivation to start and stay in treatment.","8.Seek professional help: Addiction is a complex condition that requires professional help. Consider seeing a mental health professional who specializes in addiction treatment.","9.Avoidance of triggers: Triggers such as stress, certain people or places, and emotional distress can lead to a relapse. Avoid triggers as much as possible and develop strategies to manage them as they arise.","10.Learning new coping strategies: Addiction often involves the use of substances as a coping mechanism for stress or difficult feelings. Learn new coping strategies such as mindfulness, exercise, or hobbies to manage stress and emotions in a healthy way.","11.Practice Self-Care: Taking care of your own physical and emotional needs is important for managing addiction. This can include getting enough sleep, eating a balanced diet, exercising regularly, and engaging in activities that bring you joy.","Send a message if there are any inquiries regarding these instructions or if there is difficulty in applying them."]
 PTSD = ["1.Seek professional help: Postpartum depression or anxiety is a medical condition that requires professional treatment. A mental health professional or health care provider can provide guidance and support in managing your condition.","2.Take care of your physical health: Taking care of your physical health can help improve your mental health. This can include getting enough sleep, eating a healthy diet, and getting regular physical activity.","3.Ask for help: It is important to ask for help when you need it. Don't be afraid to reach out to friends, family, or other sources of support to help with tasks like cooking, cleaning, or babysitting.","4.Attending support group meetings: Attending support group meetings can provide new moms with a safe and supportive space to share experiences and learn from others who are going through similar challenges.","5.Make time for self-care: It's important to prioritize self-care, even if it feels challenging with a new baby. This can include taking a shower, reading a book, or engaging in other activities that bring you joy and relaxation.","6.Challenge negative thoughts: When you notice negative thoughts, practice challenging them by looking for evidence to support or contradict them, and rephrase them in a more positive and realistic light.","7.Gradually increase social support: Social support is important for managing postpartum depression or anxiety. Gradually increasing social activities, such as meeting friends or joining a moms' group, can help reduce feelings of isolation.","8.Take it one day at a time: Recovering from postpartum depression or anxiety is a process, and it's important to take it one day at a time. Focus on small, achievable goals and celebrate your progress along the way.","Send a message if there are any inquiries regarding these instructions or if there is difficulty in applying them."]
@@ -22,8 +30,8 @@ schizophrenic = ["1.Therapy and Psychosocial Interventions: Engage in therapy, s
 Postpartum = ["1.Seek professional help: Postpartum depression or anxiety is a medical condition that requires professional treatment. A mental health professional or health care provider can provide guidance and support in managing your condition.","2.Take care of your physical health: Taking care of your physical health can help improve your mental health. This can include getting enough sleep, eating a healthy diet, and getting regular physical activity.","3.Ask for help: It is important to ask for help when you need it. Don't be afraid to reach out to friends, family, or other sources of support to help with tasks like cooking, cleaning, or babysitting.","4.Attending support group meetings: Attending support group meetings can provide new moms with a safe and supportive space to share experiences and learn from others who are going through similar challenges.","5.Make time for self-care: It's important to prioritize self-care, even if it feels challenging with a new baby. This can include taking a shower, reading a book, or engaging in other activities that bring you joy and relaxation.","6.Challenge negative thoughts: When you notice negative thoughts, practice challenging them by looking for evidence to support or contradict them, and rephrase them in a more positive and realistic light.","7.Gradually increase social support: Social support is important for managing postpartum depression or anxiety. Gradually increasing social activities, such as meeting friends or joining a moms' group, can help reduce feelings of isolation.","8.Take it one day at a time: Recovering from postpartum depression or anxiety is a process, and it's important to take it one day at a time. Focus on small, achievable goals and celebrate your progress along the way.","Send a message if there are any inquiries regarding these instructions or if there is difficulty in applying them."]
 def steps_print (Diagnosy,res):
     if Diagnosy == 'depreesion':
-        temp=res+depreesion
-        
+        temp = res+depreesion
+        return temp
     elif Diagnosy == 'anxiety disorder':
         for i in Anxiety :
             print(i)
@@ -45,12 +53,12 @@ def steps_print (Diagnosy,res):
     elif Diagnosy == 'education':
         for i in education_Disorder :
             print(i)
-    return temp
+
 
 @app.route("/chat", methods=["GET", "POST"])
 def chatbot_response():
     flag = 0
-    steps=0
+    steps=1
     diagnosis = {
         "depression": 0,
         "anxiety disorder": 0,
@@ -89,13 +97,14 @@ def chatbot_response():
 
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                res = np.random.choice(intent['responses'])
+                res =  np.random.choice(intent['responses'])
                 if steps == 1 :
                         res= steps_print(tag[:len(tag)-3],res)
              
         
     else:
-        res = "Please start with a meaningful word."
+        _,respon=reply(real_sentence, transformer,  tokenizer_q, tokenizer_a, "decoder_layer2_block2")
+        res = respon
 
     dict1 = {"cnt": res}
     return dict1
